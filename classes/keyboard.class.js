990931af3322fbb8de2e535521efc4970f91ba5c
ertu1k <e.kurtulmus@live.com>
2026-01-18 00:19:52 +0100

    modern keyboard

--- classes/keyboard.class.js
+++ classes/keyboard.class.js
@@ -2,11 +2,77 @@
  * Keyboard class tracking the pressed state of game control keys.
  * Used to handle player input for movement and actions.
  */
+// Update your Keyboard class
 class Keyboard {
-    LEFT = false;
-    RIGHT = false;
-    UP = false;
-    DOWN = false;
-    SPACE = false;
-    D = false;
+    constructor() {
+        this.keys = {};
+    }
 }
+
+
+let keyboard = new Keyboard();
+
+
+window.addEventListener('keydown', (e) => {
+
+    const gameKeys = ['ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown', ' ', 'd', 'D'];
+    if (gameKeys.includes(e.key)) {
+        e.preventDefault();
+    }
+    
+
+    switch(e.key) {
+        case 'ArrowRight':
+        case 'Right':
+            keyboard.RIGHT = true;
+            break;
+        case 'ArrowLeft':
+        case 'Left':
+            keyboard.LEFT = true;
+            break;
+        case 'ArrowUp':
+        case 'Up':
+            keyboard.UP = true;
+            break;
+        case 'ArrowDown':
+        case 'Down':
+            keyboard.DOWN = true;
+            break;
+        case ' ':
+            keyboard.SPACE = true;
+            break;
+        case 'd':
+        case 'D':
+            keyboard.D = true;
+            break;
+    }
+});
+
+
+window.addEventListener('keyup', (e) => {
+    switch(e.key) {
+        case 'ArrowRight':
+        case 'Right':
+            keyboard.RIGHT = false;
+            break;
+        case 'ArrowLeft':
+        case 'Left':
+            keyboard.LEFT = false;
+            break;
+        case 'ArrowUp':
+        case 'Up':
+            keyboard.UP = false;
+            break;
+        case 'ArrowDown':
+        case 'Down':
+            keyboard.DOWN = false;
+            break;
+        case ' ':
+            keyboard.SPACE = false;
+            break;
+        case 'd':
+        case 'D':
+            keyboard.D = false;
+            break;
+    }
+});
